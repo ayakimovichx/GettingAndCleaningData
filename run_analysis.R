e9da0f8424fda875339c21ac137b7a62cf4907d0
ayakimovichz <xiddix@gmail.com>
2015-05-11 16:29:51 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -18,9 +18,8 @@ col.names=c("number", "activity"), stringsAsFactors=FALSE)
 print("loading and processing train data...", quote=FALSE)
 x_train<-read.csv("./data/UCI HAR Dataset/train/X_train.txt", header=FALSE, sep="", col.names=features_col)
 x_train<-x_train[,c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,
-165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,294,295,296,345,346,347,348,349,350,
-373,374,375,424,425,426,427,428,429,452,453,454,503,504,513,516,517,526,529,530,539,542,543,552,555,556,557,
-558,559,560,561)]
+165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,
+424,425,426,427,428,429,503,504,516,517,529,530,542,543)]
 x_train$id<-1:nrow(x_train)
 
 y_train<-read.csv("./data/UCI HAR Dataset/train/y_train.txt", header=FALSE, sep="", col.names=c("number"))
@@ -39,9 +38,8 @@ m_train<-merge(m_train_t, x_train, by=c("id"), sort=FALSE)
 print("loading and processing test data...", quote=FALSE)
 x_test<-read.csv("./data/UCI HAR Dataset/test/X_test.txt", header=FALSE, sep="", col.names=features_col)
 x_test<-x_test[,c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,
-165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,294,295,296,345,346,347,348,349,350,
-373,374,375,424,425,426,427,428,429,452,453,454,503,504,513,516,517,526,529,530,539,542,543,552,555,556,557,
-558,559,560,561)]
+165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,
+424,425,426,427,428,429,503,504,516,517,529,530,542,543)]
 x_test$id<-1:nrow(x_test)
 y_test<-read.csv("./data/UCI HAR Dataset/test/y_test.txt", header=FALSE, sep="", col.names=c("number"))
 y_test$id<-1:nrow(y_test)
@@ -80,7 +78,7 @@ rm(m_test)
 unlink("./data", recursive=TRUE)
 
 ## FINAL DATASET OF MEANS
-m_mean<-aggregate(m_merged[,4:89], by=list(activity=m_merged$activity, subject=m_merged$subject), 
+m_mean<-aggregate(m_merged[,4:69], by=list(activity=m_merged$activity, subject=m_merged$subject), 
 FUN=mean, na.rm=TRUE)
 print("writing final dataset into './tidy_dataset.txt'...", quote=FALSE)
 write.table(m_mean, "./tidy_dataset.txt", row.names=FALSE)
